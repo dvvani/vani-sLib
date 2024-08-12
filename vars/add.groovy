1d683bd641d1f40229493ea8fabe10a7efd77b84
dvvani <138096360+dvvani@users.noreply.github.com>
2024-08-12 18:52:46 +0530

    Update add.groovy

--- vars/add.groovy
+++ vars/add.groovy
@@ -1,5 +1,5 @@
-def call (X,Y){
-  X = X as Integer
-  Y = Y as Integer
-  echo "sum of ${X} and ${Y} is ${X+Y}"
+def call(X, Y) {
+    X = (X != null) ? X as Integer : 0
+    Y = (Y != null) ? Y as Integer : 0
+    echo "Sum of ${X} and ${Y} is ${X + Y}"
 }
